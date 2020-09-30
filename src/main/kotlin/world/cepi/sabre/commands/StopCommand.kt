09ea141e28b3c5a5eaba56e72584ab5044bfda9e
CmdrNorthpaw <thetuxkid@gmail.com>
2020-09-30 16:55:13 +0100

    Added stop command

---
+++ src/main/kotlin/world/cepi/sabre/commands/StopCommand.kt
@@ -0,0 +1,24 @@
+package world.cepi.sabre.commands
+
+import net.minestom.server.MinecraftServer
+import net.minestom.server.command.CommandProcessor
+import net.minestom.server.command.CommandSender
+import net.minestom.server.command.builder.Arguments
+import net.minestom.server.command.builder.CommandExecutor
+import net.minestom.server.entity.Player
+
+object StopCommand : CommandProcessor {
+    override fun getCommandName(): String = "stop"
+
+    override fun getAliases(): Array<String> = arrayOf()
+
+    override fun process(sender: CommandSender?, command: String?, args: Array<out String>?): Boolean {
+        MinecraftServer.stopCleanly()
+        return true
+    }
+
+    override fun hasAccess(player: Player?): Boolean = player!!.permissionLevel >= 4
+}
+
+
+
