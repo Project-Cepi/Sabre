378dff21e6008322f96dc77b061fcc8bd03e6a42
LeoDog896 <LeoDog896@hotmail.com>
2020-09-29 20:17:06 -0400

    Flexible flat world configuration

--- src/main/kotlin/world/cepi/sabre/instances/generators/flat/Flat.kt
+++ src/main/kotlin/world/cepi/sabre/instances/generators/flat/Flat.kt
@@ -5,16 +5,28 @@ import net.minestom.server.instance.Chunk
 import net.minestom.server.instance.ChunkGenerator
 import net.minestom.server.instance.ChunkPopulator
 import net.minestom.server.instance.batch.ChunkBatch
-import net.minestom.server.instance.block.Block
 import net.minestom.server.world.biomes.Biome
 import java.util.*
 
+/**
+ * Flat world generation based off of [ChunkGenerator]
+ */
+class Flat(
+        /**
+         * List of [FlatLayer]s used to generate the flat world.
+         */
+        private vararg val layers: FlatLayer,
 
-class Flat(private vararg val layers: FlatLayer) : ChunkGenerator {
+        /**
+         * List of chunk populaters to be used for chunk generation.
+         */
+        private val chunkPopulators: List<ChunkPopulator> = listOf(),
+) : ChunkGenerator {
+
     override fun generateChunkData(batch: ChunkBatch, chunkX: Int, chunkZ: Int) {
         for (x in 0 until Chunk.CHUNK_SIZE_X) for (z in 0 until Chunk.CHUNK_SIZE_Z) {
             var y = 0
-            for (layer in layers) for (yLoop in y until y + layer.height) {
+            for (layer in layers) for (yLoop in y until y + layer.thickness) {
                 batch.setBlock(x, y, z, layer.block)
                 y++;
             }
@@ -25,7 +37,5 @@ class Flat(private vararg val layers: FlatLayer) : ChunkGenerator {
         Arrays.fill(biomes, MinecraftServer.getBiomeManager().getById(0))
     }
 
-    override fun getPopulators(): List<ChunkPopulator?>? {
-        return null
-    }
+    override fun getPopulators() = chunkPopulators
 }
