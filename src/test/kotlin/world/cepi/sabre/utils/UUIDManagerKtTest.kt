64111f35fdcb157691ec5b8cbe0dafd2d2662477
LeoDog896 <LeoDog896@hotmail.com>
2020-10-01 13:21:50 -0400

    NumberFormatException test

--- src/test/kotlin/world/cepi/sabre/utils/UUIDManagerKtTest.kt
+++ src/test/kotlin/world/cepi/sabre/utils/UUIDManagerKtTest.kt
@@ -1,5 +1,6 @@
 package world.cepi.sabre.utils
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.util.*
@@ -10,5 +11,14 @@ class UUIDManagerKtTest {
         val uuid = UUID.randomUUID()
 
         assertEquals(toValidUuid(uuid.toString().replace("-", "")), uuid)
+    }
+
+    @Test
+    fun `should throw NumberFormatException if not perfect`() {
+        assertThrows(NumberFormatException::class.java) {
+            val uuid = UUID.randomUUID()
+
+            assertNotEquals(toValidUuid(uuid.toString()), uuid)
+        }
     }
 }
