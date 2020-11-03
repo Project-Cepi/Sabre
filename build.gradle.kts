fa0683a154702eb6c4ea9abcf3eae2054d6e3a6e
benjables <xbenjablesx@gmail.com>
2020-11-03 17:13:32 -0500

    finished kick

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
 
     // Compile Minestom into project
-    implementation("com.github.Minestom:Minestom:463e1f047f")
+    implementation("com.github.Minestom:Minestom:b575d6df57")
 
     // Use the Netty library
     implementation("io.netty:netty-transport-native-epoll:4.1.52.Final")
