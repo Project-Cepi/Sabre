b390491b8b66144fef1ce07e08e646d8c85cd9f3
LeoDog896 <LeoDog896@hotmail.com>
2020-11-10 18:22:12 -0500

    Add entity and render distance

--- src/main/kotlin/world/cepi/sabre/loaders/ViewLoader.kt
+++ src/main/kotlin/world/cepi/sabre/loaders/ViewLoader.kt
@@ -1,11 +1,13 @@
 package world.cepi.sabre.loaders
 
 import net.minestom.server.MinecraftServer
+import world.cepi.sabre.Config.Companion.config
 
 /** Loads view distances of both entities and chunks */
 object ViewLoader : Loader {
 
     override fun load() {
-
+        MinecraftServer.setEntityViewDistance(config.entityDistance)
+        MinecraftServer.setChunkViewDistance(config.renderDistance)
     }
 }
