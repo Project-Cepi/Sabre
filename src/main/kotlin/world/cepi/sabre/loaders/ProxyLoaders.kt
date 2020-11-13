74e451b14326e8a83ddafb89a32efe6a6a418450
LeoDog896 <LeoDog896@hotmail.com>
2020-11-13 18:40:08 -0500

    Use kotlin getters for proxy

--- src/main/kotlin/world/cepi/sabre/loaders/ProxyLoaders.kt
+++ src/main/kotlin/world/cepi/sabre/loaders/ProxyLoaders.kt
@@ -5,22 +5,24 @@ import net.minestom.server.extras.velocity.VelocityProxy
 import world.cepi.sabre.Forwarder
 import world.cepi.sabre.config
 
-val config = config()
-var proxy: Forwarder = Forwarder.NONE
+val proxy: Forwarder
+    get() {
+        return try {
+            Forwarder.valueOf(config().proxy.toUpperCase())
+        } catch(exception: Exception) {
+            println("ERROR! Configured proxy does not match one of the following: BungeeCord, Velocity, None")
+            Forwarder.NONE
+        }
+    }
 
 object VelocityLoader : Loader {
     override fun load() {
-        if (checkConfig() && proxy == Forwarder.VELOCITY ) VelocityProxy.enable(config().velocitySecret)
+        if (proxy == Forwarder.VELOCITY) VelocityProxy.enable(config().velocitySecret)
     }
 }
 
 object BungeeLoader : Loader {
     override fun load() {
-        if (checkConfig() && proxy == Forwarder.BUNGEE) BungeeCordProxy.enable()
+        if (proxy == Forwarder.BUNGEE) BungeeCordProxy.enable()
     }
-}
-
-private fun checkConfig(): Boolean = try {proxy = Forwarder.valueOf(config.proxy.toUpperCase()); true}
-catch (e: IllegalArgumentException) {
-    println("ERROR! Configured proxy does not match one of the following: BungeeCord, Velocity, None"); false
 }
