6a991d15072d6f74edc82e78aad4bdf5041cb482
LeoDog896 <LeoDog896@hotmail.com>
2020-11-28 19:50:13 -0500

    Update minestom

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
     implementation(kotlin("reflect"))
 
     // Compile Minestom into project
-    implementation("com.github.Minestom", "Minestom", "a8c1c73")
+    implementation("com.github.Minestom", "Minestom", "1f2e4de0b3")
 
     // OkHttp
     implementation("com.squareup.okhttp3", "okhttp", "4.9.0")
