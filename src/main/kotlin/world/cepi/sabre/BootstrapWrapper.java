87abdde7d0b880553ae82de4ab59a9e44ae0527b
LeoDog896 <LeoDog896@hotmail.com>
2020-12-28 11:49:04 -0500

    Inline JavaDocs

--- src/main/kotlin/world/cepi/sabre/BootstrapWrapper.java
+++ src/main/kotlin/world/cepi/sabre/BootstrapWrapper.java
@@ -2,9 +2,7 @@ package world.cepi.sabre;
 
 import net.minestom.server.Bootstrap;
 
-/**
- * Bootstrap wrapper for Minestom. Written in java to prevent Kotlin Bootstrap errors
- */
+/** Bootstrap wrapper for Minestom. Written in java to prevent Kotlin Bootstrap errors */
 public class BootstrapWrapper {
 
     public static void main(String[] args) {
