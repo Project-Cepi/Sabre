b704021ab3ec29c10371dcd9842724403ea52ae0
LeoDog896 <LeoDog896@hotmail.com>
2021-01-08 08:10:50 -0500

    Bump minestom

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     implementation(kotlin("reflect"))
 
     // Compile Minestom into project
-    implementation("com.github.Minestom", "Minestom", "e4e564ab07")
+    implementation("com.github.Minestom", "Minestom", "066b3dc417")
 
     // OkHttp
     implementation("com.squareup.okhttp3", "okhttp", "4.9.0")
