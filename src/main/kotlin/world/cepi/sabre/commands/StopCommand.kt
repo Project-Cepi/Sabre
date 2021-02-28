cce93f7b174e4fd5af3193843cc25aa982d1078a
LeoDog896 <LeoDog896@hotmail.com>
2021-02-28 11:25:50 -0500

    Allow calling of /stop in the console

--- src/main/kotlin/world/cepi/sabre/commands/StopCommand.kt
+++ src/main/kotlin/world/cepi/sabre/commands/StopCommand.kt
@@ -3,6 +3,7 @@ package world.cepi.sabre.commands
 import net.minestom.server.MinecraftServer
 import net.minestom.server.command.CommandProcessor
 import net.minestom.server.command.CommandSender
+import net.minestom.server.command.ConsoleSender
 import net.minestom.server.entity.Player
 import world.cepi.kstom.command.KommandProcessor
 
@@ -11,7 +12,7 @@ class ShutdownCommand : KommandProcessor("stop",
         listOf(),
         { it.permissionLevel >= 4 || it.hasPermission("sabre.stop") },
         lambda@{ sender, _, _ ->
-            if (sender.hasPermission("sabre.stop") || (sender is Player && sender.permissionLevel >= 4)) {
+            if (sender.hasPermission("sabre.stop") || (sender is Player && sender.permissionLevel >= 4) || (sender is ConsoleSender)) {
                 MinecraftServer.stopCleanly()
                 return@lambda true
             }
