7fa0418118f63be7c086334a8680f07142c0c7d8
LeoDog896 <LeoDog896@hotmail.com>
2021-03-25 10:45:34 -0400

    Bump Minestom

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
 
     // Compile Minestom into project
-    implementation("com.github.Project-Cepi", "Minestom", "8a113b72eb")
+    implementation("com.github.Project-Cepi", "Minestom", "13b1d1e7a5")
 
     // org.json
     implementation("org.json", "json", "20200518")
