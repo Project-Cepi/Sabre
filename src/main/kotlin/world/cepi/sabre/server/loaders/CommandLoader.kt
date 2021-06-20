3c1e89ce269ca070903122612686d7cfc2b1b8d6
LeoDog896 <LeoDog896@hotmail.com>
2021-06-20 15:25:54 -0400

    *actually fix the build

--- src/main/kotlin/world/cepi/sabre/server/loaders/CommandLoader.kt
+++ src/main/kotlin/world/cepi/sabre/server/loaders/CommandLoader.kt
@@ -2,10 +2,9 @@ package world.cepi.sabre.server.loaders
 
 import com.google.common.base.Strings
 import net.kyori.adventure.text.Component
+import net.minestom.server.MinecraftServer
 import net.minestom.server.command.CommandSender
 import net.minestom.server.utils.callback.CommandCallback
-import world.cepi.kstom.Manager
-import world.cepi.kstom.command.register
 import world.cepi.sabre.server.commands.security.DeopCommand
 import world.cepi.sabre.server.commands.security.OpCommand
 import world.cepi.sabre.server.Config.Companion.config
@@ -14,7 +13,7 @@ import world.cepi.sabre.server.commands.StopCommand
 internal fun commandLoader() {
 
     if (!Strings.isNullOrEmpty(config.unknownMessage)) {
-        Manager.command.unknownCommandCallback =
+        MinecraftServer.getCommandManager().unknownCommandCallback =
             CommandCallback { sender: CommandSender, command: String ->
                 if (!Strings.isNullOrEmpty(command)) {
                     sender.sendMessage(Component.text(config.unknownMessage))
@@ -22,11 +21,11 @@ internal fun commandLoader() {
             }
     }
 
-    StopCommand.register()
+    MinecraftServer.getCommandManager().register(StopCommand)
 
     if (config.opUtilities) {
-        OpCommand.register()
-        DeopCommand.register()
+        MinecraftServer.getCommandManager().register(OpCommand)
+        MinecraftServer.getCommandManager().register(DeopCommand)
     }
 
 
