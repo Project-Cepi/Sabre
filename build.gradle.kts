c8a9e912079cdad478d0a0c526a205d5591082d6
LeoDog896 <LeoDog896@hotmail.com>
2021-09-11 17:29:22 -0400

    Bump minestom

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
 
     // Compile Minestom into project
-    implementation("com.github.Minestom", "Minestom", "d7a116c144")
+    implementation("com.github.Minestom", "Minestom", "e77e32889b")
 
     implementation("org.apache.logging.log4j:log4j-jul:2.14.1")
 
