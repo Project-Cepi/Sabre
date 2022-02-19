228c6cd79f87f52de8e7d2a5f394e00ec5b83aa1
Tristan <LeoDog896@hotmail.com>
2022-02-19 18:32:28 -0500

    Remove Strings reference

--- src/main/kotlin/world/cepi/sabre/server/loaders/CommandLoader.kt
+++ src/main/kotlin/world/cepi/sabre/server/loaders/CommandLoader.kt
@@ -1,6 +1,5 @@
 package world.cepi.sabre.server.loaders
 
-import com.google.common.base.Strings
 import net.kyori.adventure.text.minimessage.MiniMessage
 import net.kyori.adventure.text.Component
 import net.minestom.server.MinecraftServer
@@ -14,10 +13,10 @@ import world.cepi.sabre.server.commands.UpdateCommand
 
 internal fun commandLoader() {
 
-    if (!Strings.isNullOrEmpty(config.unknownMessage)) {
+    if (config.unknownMessage != "") {
         MinecraftServer.getCommandManager().unknownCommandCallback =
             CommandCallback { sender: CommandSender, command: String ->
-                if (!Strings.isNullOrEmpty(command)) {
+                if (command != "") {
                     sender.sendMessage(MiniMessage.get().parse(config.unknownMessage))
                 }
             }
